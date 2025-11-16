9b1b18c2ad57c6c787cb777c511f087efd5fe7df
himel2535 <monwarhossanhimel@gmail.com>
2025-11-16 16:09:12 +0600

    customize theme

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -4,8 +4,9 @@ import logo from "../../assets/logo.png";
 
 const Logo = () => {
   return (
-    <div>
+    <div className="flex items-end">
       <img src={logo} alt="" />
+      <h3 className="text-3xl font-bold -ms-2.5 -mb-0.5">ZapShift</h3>
     </div>
   );
 };
