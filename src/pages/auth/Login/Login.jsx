c9d30124387f83202863afef275708efe05a77f6
himel2535 <monwarhossanhimel@gmail.com>
2025-11-18 00:14:29 +0600

    create login page and use custom hook

--- src/pages/auth/Login/Login.jsx
+++ src/pages/auth/Login/Login.jsx
@@ -1,11 +1,75 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import useAuth from "../../../hooks/useAuth";
 
 const Login = () => {
-    return (
-        <div>
-            this is login and login
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { signInUser } = useAuth();
+
+  const handleSignIn = (data) => {
+
+    signInUser(data.email, data.password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <form onSubmit={handleSubmit(handleSignIn)} className="card-body">
+        <fieldset className="fieldset">
+          {/* ---Email--- */}
+          <label className="label">Email</label>
+          <input
+            {...register("email", {
+              required: true,
+            })}
+            type="email"
+            className="input"
+            placeholder="Email"
+          />
+
+          {errors.email?.type === "required" && (
+            <p className="text-red-500">Email is required</p>
+          )}
+
+          {/* ---Password--- */}
+          <label className="label">Password</label>
+          <input
+            {...register("password", {
+              required: true,
+              pattern:
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/,
+            })}
+            type="password"
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required</p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p className="text-red-500">
+              Password must have at least one uppercase,at least one lowercase,
+              at least one digit and at least one special character
+            </p>
+          )}
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
